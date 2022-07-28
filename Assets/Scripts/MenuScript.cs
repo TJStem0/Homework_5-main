5dba392fdca815b4f3ad7933a5dd739806842857
Tony <108951239+TJStem0@users.noreply.github.com>
2022-07-28 13:17:24 -0400

    Add project files.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public string levelName;
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
